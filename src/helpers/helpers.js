8579eeb2ea7574013118b658b699c4e7575c8488
aanmeba <aanmeba@gmail.com>
2023-07-11 19:48:19 +1000

    add cleaning data function

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,3 +1,5 @@
+import imgUrl from "../assets/bookcover.png";
+
 export const addEllipsis = (str, limit) => {
   if (str.length > limit) {
     return str.slice(0, limit) + "...";
@@ -40,15 +42,78 @@ export const handleEscapedChars = (str) => {
 };
 
 export const printAuthors = (array) => {
-  return array
-    .map((author, i, arr) =>
-      i === 0 ? author : i === arr.length - 1 ? `and ${author}` : `, ${author}`
-    )
-    .join(" ");
+  let string = "";
+  array.forEach((author, i, arr) => {
+    if (i === 0) string += author;
+    if (i === arr.length - 1) string += ` and ${author}`;
+    else {
+      string.trim();
+      string += `, ${author}`;
+    }
+  });
+
+  return string;
 };
 
 export const handleReviews = (num) => {
   let str = "";
   num / 1000 > 1 ? (str += (num / 1000).toFixed(2) + "k") : (str += num);
   return num > 0 ? (str += ` reviews`) : ` review`;
+};
+
+export const cleanedData = (book) => {
+  const {
+    id,
+    saleInfo: { isEbook },
+    volumeInfo: {
+      authors,
+      averageRating,
+      categories,
+      description,
+      imageLinks: { thumbnail = "" } = {},
+      infoLink,
+      publisher,
+      publishedDate,
+      title,
+      subtitle,
+      pageCount,
+      ratingsCount = 0,
+    },
+  } = book;
+
+  let authorsInModal,
+    authorsInList = authors && authors[0],
+    publishingInfo = "";
+
+  if (authors?.length > 1) {
+    authorsInModal = printAuthors(authors);
+    authorsInList += " and more";
+  }
+
+  if (publishedDate) {
+    publishingInfo += formatePublishedDate(publishedDate);
+  }
+
+  if (publisher) {
+    publishingInfo += ` · ${publisher}`;
+  }
+
+  const desc = description ? description : "Description is not available";
+  const bookCover = thumbnail ? thumbnail : imgUrl;
+
+  return {
+    id,
+    isEbook,
+    authors: { authorsInList, authorsInModal },
+    averageRating,
+    description: desc,
+    categories,
+    thumbnail: bookCover,
+    publishingInfo,
+    infoLink,
+    title,
+    subtitle,
+    pageCount,
+    ratingsCount,
+  };
 };
