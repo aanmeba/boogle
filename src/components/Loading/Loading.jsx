647f246739ae8b03ce81843807b72426f42dad5f
aanmeba <aanmeba@gmail.com>
2023-07-13 15:06:25 +1000

    fix loading grid layout

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,11 +1,9 @@
-import { Fragment } from "react";
 import { Skeleton } from "@mui/material";
-import styles from "./Loading.module.scss";
 
 const Loading = () => {
   const dummyList = Array.from(new Array(5));
   return (
-    <Fragment className={styles.container}>
+    <>
       {dummyList.map((_, index) => (
         <div key={index}>
           <Skeleton
@@ -20,7 +18,7 @@ const Loading = () => {
           <Skeleton width="60%" />
         </div>
       ))}
-    </Fragment>
+    </>
   );
 };
 
