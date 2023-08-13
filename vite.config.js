2479c26d5067e5f80aa4c2b454254cceea6e3729
aanmeba <aanmeba@gmail.com>
2023-08-13 12:04:06 +1000

    reset the config for gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
-    base: "/boogle/",
     plugins: [react()],
     test: {
       globals: true,
@@ -12,10 +11,6 @@ export default defineConfig(({ command }) => {
       setupFiles: "./config/test-setup.js",
     },
   };
-
-  if (command !== "serve") {
-    config.base = "/boogle/";
-  }
 
   return config;
 });
